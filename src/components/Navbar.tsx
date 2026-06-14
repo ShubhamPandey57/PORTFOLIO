4fa67718ed54cc5738a4802e85ff57e0250209ba
ShubhamPandey57 <shubhampandey2869@gmail.com>
2026-06-14 21:46:53 +0530

    navbar fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -135,7 +135,7 @@ export default function Navbar() {
       <AnimatePresence>
         {isMobileMenuOpen && (
           <motion.div
-            initial={{ opacity: 0, y: -2 }}
+            initial={{ opacity: 0, y: -20 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -20 }}
             transition={{ duration: 0.3 }}
