0bc9611b1a2599f06831582adcbde814fd791461
ShubhamPandey57 <shubhampandey2869@gmail.com>
2026-06-25 20:42:01 +0530

    workin in projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -28,7 +28,7 @@ interface Project {
   image: string;
   github: string;
   live: string;
-  color: "gold" | "violet" | "emerald";
+  color: "gold" | "viole" | "emerald";
 }
 
 const projectsData: Project[] = [
