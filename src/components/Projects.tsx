9740026b7033ace36cbd405e89971772f16aa581
ShubhamPandey57 <shubhampandey2869@gmail.com>
2026-06-25 20:41:50 +0530

    workin in projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -23,7 +23,7 @@ const GithubIcon = (props: React.SVGProps<SVGSVGElement>) => (
 interface Project {
   title: string;
   rank: "S-Rank" | "A-Rank" | "B-Rank";
-  description: str;
+  description: string;
   techStack: string[];
   image: string;
   github: string;
